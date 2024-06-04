b71dafc6068f2b5ba334b74518b033f0cf0b9523
Muspi Merol <me@promplate.dev>
2024-06-04 06:20:36 +0000

    temp

--- src/tests/test_static_typing.py
+++ src/tests/test_static_typing.py
@@ -1,76 +1,40 @@
-from inspect import getsource
-from pathlib import Path
-from textwrap import dedent
-from typing import TypeVar, assert_type
+from typing import TypeVar
 
-from mypy.api import run
-from pytest import raises
+import pytest
+
+from pyodide.ffi import JsDoubleProxy, JsPromise, create_once_callable, create_proxy
 
 
-def _mypy_check(source: str) -> tuple[str, str, int]:
-    path = str(Path(__file__).parent.parent.parent / "pyproject.toml")
-    stdout, stderr, exit_status = run(["-c", source, "--config-file", path])
-    return stdout, stderr, exit_status
-
-
-def assert_no_error(source: str) -> None:
-    stdout, _, exit_status = _mypy_check(f"from typing import *\n\n{dedent(source)}")
-    assert exit_status == 0, stdout
-
-
-def test_self():
-    with raises(AssertionError):
-        assert_no_error("a: str = 123")
-
-
+@pytest.mark.mypy_testing
 def test_create_proxy():
-    def _():
-        from pyodide.ffi import create_proxy
-
-        a: int = 2
-        assert_type(create_proxy(a).unwrap(), int)
-
-    assert_no_error(getsource(_))
+    _: int = create_proxy(42).unwrap()
 
 
+@pytest.mark.mypy_testing
 def test_generic():
-    def _():
-        from pyodide.ffi import JsDoubleProxy, JsPromise
+    async def _(a: JsPromise[int]) -> str:
+        return await a.then(str)  # type:ignore[misc]
 
-        def _(a: JsPromise[int]) -> None:
-            assert_type(a.then(str), JsPromise[str])
-
-        def _(b: JsDoubleProxy[str]) -> None:
-            assert_type(b.unwrap(), str)
-
-    assert_no_error(getsource(_))
+    def _(b: JsDoubleProxy[str]) -> str:
+        return b.unwrap()
 
 
+@pytest.mark.mypy_testing
 def test_callable_generic():
-    def _():
-        from pyodide.ffi import create_once_callable, create_proxy
+    T = TypeVar("T", int, float, str)
 
-        T = TypeVar("T", int, float, str)
+    def f(x: T) -> T:
+        return x * 2
 
-        def f(x: T) -> T:
-            return x * 2
-
-        assert_type(create_once_callable(f)(2), int)
-        assert_type(create_proxy(f)(""), str)
-
-    assert_no_error(getsource(_))
+    _: tuple[int, str] = create_once_callable(f)(2), create_proxy(f)("")
 
 
+@pytest.mark.mypy_testing
 def test_decorator_usage():
-    def _():
-        from pyodide.ffi import create_once_callable
+    T = TypeVar("T")
 
-        T = TypeVar("T")
+    @create_once_callable
+    def f(x: T) -> list[T]:
+        return [x]
 
-        @create_once_callable
-        def f(x: T) -> list[T]:
-            return [x]
-
-        assert_type(f((int(input()), str(input()))), list[tuple[int, str]])
-
-    assert_no_error(getsource(_))
+    _: list[tuple[int, str]] = f((int("1"), str(1)))
